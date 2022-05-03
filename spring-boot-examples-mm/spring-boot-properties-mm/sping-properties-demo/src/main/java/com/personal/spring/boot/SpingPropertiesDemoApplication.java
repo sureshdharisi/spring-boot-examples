b8c72f22215966e41060aaa9611872ad1173f8e5
sureshdharisi <suresh.dharisi@gmail.com>
2022-05-03 15:49:50 -0400

    adding properties files

---
+++ spring-boot-examples-mm/spring-boot-properties-mm/sping-properties-demo/src/main/java/com/personal/spring/boot/SpingPropertiesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.personal.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingPropertiesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingPropertiesDemoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-examples-mm/spring-boot-properties-mm/sping-properties-demo/src/test/java/com/personal/spring/boot/SpingPropertiesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingPropertiesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
